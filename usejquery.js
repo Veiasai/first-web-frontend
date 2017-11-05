8cad05d601c512134af3cf0ba521723981fe27e7
Veiasai <2934456168@qq.com>
2017-11-05 22:30:05 +0800

    tuling api

--- usejquery.js
+++ usejquery.js
@@ -1,3 +1,16 @@
 /**
  * Created by Administrator on 2017/11/5.
  */
+$(document).ready(function(){
+    $("button").click(function(){
+        $.post("http://www.tuling123.com/openapi/api",
+            {
+                "key": "1a1d8f62e7db4e9ca94f014d89e29675",
+                "info": $("input[name='info']").val(),
+                "userid" :"11"
+            },
+            function (result){
+                $("input[name='out']").val(result["text"])
+        })
+    });
+});
